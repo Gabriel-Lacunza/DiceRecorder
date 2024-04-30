f60692cb16ac22349e5ef18cdd347d5250bd1041
GabrielLacunza <143532488+GabrielLacunza@users.noreply.github.com>
2024-04-30 15:48:16 -0400

    feat: create campaing page created

--- App.tsx
+++ App.tsx
@@ -4,12 +4,13 @@ import CampaingSelectionPage from './components/pages/CampaingSelectionPage';
 import CampaingLuckPage from './components/pages/CampaingLuckPage';
 import DiceLuckPage from './components/pages/DiceLuckPage';
 import DiceCalculatorPage from './components/pages/DiceCalculatorPage';
+import CreateCampaingPage from './components/pages/CreateCampaingPage';
 
 export default function App() {
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
 			<StatusBar style="auto" />
-			<DiceLuckPage />
+			<CreateCampaingPage />
 		</SafeAreaView>
 	);
 }
--- components/organisms/index.ts
+++ components/organisms/index.ts
@@ -1,3 +1,5 @@
 export { DiceRows } from './CampaingLuckPage/DiceRows';
 export { Calculator } from './DiceCalculatorPage/Calculator';
 export { FaceRows } from './DiceLuckPage/FaceRows';
+export { CakeGraph } from './DiceLuckPage/CakeGraph';
+export { SelectDiceSet } from './CreateCampaing/SelectDiceSet';
